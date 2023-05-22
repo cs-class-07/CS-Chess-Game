be701c92e98599babf6a170a1fa440207e912297
robigan <35210888+robigan@users.noreply.github.com>
2023-05-22 14:36:48 +0200

    Yessir

--- main.cpp
+++ main.cpp
@@ -4,10 +4,24 @@
 
 using namespace std;
 
-struct Chess
+enum class Piece {
+    PAWN,
+    ROOK,
+    NIGHT, // Intentional spelling mistake
+    BISHOP,
+    QUEEN,
+    KING,
+    NOTHING
+};
+
+class Chess
 {
+private:
+    string playerNames[2];
+    bool playingPlayer = false; // False is bottom player, true is top player
+
+public:
     char board[8][8];
-    string player_names[2];
 
     Chess()
     {
@@ -19,23 +33,35 @@ struct Chess
             }
         }
 
-        player_names[0] = "no name";
-        player_names[1] = "no name";
+        playerNames[0] = "player blue";
+        playerNames[1] = "player red";
+    }
+
+    bool getPlayingPlayer() {
+        return playingPlayer;
+    }
+
+    string getPlayingPlayerWithName() {
+        return playerNames[playingPlayer];
+    }
+
+    void switchPlayer() {
+        playingPlayer = !playingPlayer;
     }
 
     void initialize()
     {
-        string player1;
-        string player2;
+        // string player1;
+        // string player2;
         
-        cout << "Player 1 name: ";
-        cin >> player1;
+        // cout << "Player 1 name: ";
+        // cin >> player1;
 
-        cout << "Player 2 name: ";
-        cin >> player2;
+        // cout << "Player 2 name: ";
+        // cin >> player2;
         
-        player_names[0] = player1;
-        player_names[2] = player2;
+        // playerNames[0] = player1;
+        // playerNames[1] = player2;
 
         char layout1[] = "rnbqkbnr";
         char layout2[] = "pppppppp";
@@ -64,15 +90,15 @@ struct Chess
         }
     }
 
-    void move(const int x, const int y, const int i, const int j) {
-        board[i][j] = board[x][y];
-        board[x][y] = ' ';
+    void move(const tuple<int, int, int, int> &input) {
+        board[get<2>(input)][get<3>(input)] = board[get<0>(input)][get<1>(input)];
+        board[get<0>(input)][get<1>(input)] = ' ';
     }
 
     void print_players()
     {
-        cout << "Player 1's name is " << player_names[0] << endl
-             << "Player 2's name is " << player_names[1] << endl;
+        cout << "Player 1's name is " << playerNames[0] << endl
+             << "Player 2's name is " << playerNames[1] << endl;
     }
 };
 
@@ -107,19 +133,20 @@ private:
     }
 
 public:
-    void render(const char (&board)[8][8]) {
+    void render() {
         render_header_row();
 
         for (int i = 0; i < 8; i++) {
-            render_data_row(board[i], i);
+            render_data_row(chess->board[i], i);
         }
 
         render_footer_row();
     }
 
-    void ask_for_movement(Chess &chessController) {
+    tuple<int, int, int, int> ask_for_movement() {
         string piece;
         string moveTo;
+
         int x = 0;
         int y = 0;
         int i = 0;
@@ -127,40 +154,100 @@ public:
 
         cout << "Piece to move: ";
         cin >> piece;
-        cout << "Move to: ";
-        cin >> moveTo;
-
         if (97 <= piece[0] && piece[0] <= 104) y = piece[0] - 97;
         if (49 <= piece[1] && piece[1] <= 57) x = piece[1] - 49;
+
+        if (chess->board[x][y] == ' ') {
+            cout << "No piece at position" << endl;
+            return this->ask_for_movement();
+        }
+
+        cout << "Move to: ";
+        cin >> moveTo;
 
         if (97 <= moveTo[0] && moveTo[0] <= 104) j = moveTo[0] - 97;
         if (49 <= moveTo[1] && moveTo[1] <= 57) i = moveTo[1] - 49;
 
-        chessController.move(x,y,i,j);
+        return make_tuple(x, y, i, j);
     }
 
-    
-    Renderer(Chess c) {
+    Renderer(Chess &c) {
+        chess = &c;
+    }
+};
+
+class Engine {
+private:
+    Chess * chess;
+    int preRow;
+    int preCol;
+    int postRow;
+    int postCol;
+
+    bool checkPawn() {
+        if (chess->getPlayingPlayer() == 0) {
+            if (postRow + 1 != preRow) return false;
+        } else {
+            if (postRow - 1 != preRow) return false;
+        }
+
+        if (preCol == postCol && chess->board[postRow][postCol] != ' ') return false;
+        if (preCol != postCol && chess->board[postRow][postCol] == ' ') return false;
+        return true;
+    }
+
+public:
+    bool checkMovement(const tuple<int, int, int, int> &input) {
+        preRow = get<0>(input);
+        preCol = get<1>(input);
+        postRow = get<2>(input);
+        postCol = get<3>(input);
+
+        Piece piece;
+        char preConversion = (*chess).board[preRow][preCol];
+        if (preConversion == 'p' || preConversion == 'P') piece = Piece::PAWN;
+        if (preConversion == 'r' || preConversion == 'R') piece = Piece::ROOK;
+        if (preConversion == 'n' || preConversion == 'N') piece = Piece::NIGHT;
+        if (preConversion == 'b' || preConversion == 'B') piece = Piece::BISHOP;
+        if (preConversion == 'q' || preConversion == 'Q') piece = Piece::QUEEN;
+        if (preConversion == 'k' || preConversion == 'K') piece = Piece::KING;
+
+        if (piece == Piece::PAWN && !checkPawn()) return false;
+        return true;
+    }
+
+    Engine(Chess &c) {
         chess = &c;
     }
 };
 
 int main()
 {
-    Chess C;
-    Renderer renderer(C);
+    Chess chess;
+    Renderer renderer(chess);
+    Engine engine(chess);
 
-    C.initialize();
+    chess.initialize();
 
     cout << endl;
 
-    C.print_players();
+    chess.print_players();
 
-    renderer.render(C.board);
+    renderer.render();
 
     while (true) {
-        renderer.ask_for_movement(C);
-        renderer.render(C.board);
+        cout << "Current player: " << chess.getPlayingPlayerWithName() << endl;
+
+        tuple<int, int, int, int> result = renderer.ask_for_movement();
+        if (engine.checkMovement(result)) {
+            chess.move(result);
+            renderer.render();
+            chess.switchPlayer();
+        } else {
+            cout << "Invalid movement!" << endl;
+        }
+
+        cout << endl << endl;
     }
 
     return 0;
