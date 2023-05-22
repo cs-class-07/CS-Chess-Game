e5921c5e9ba099ba5433f6ef5f80f087e1c5846d
robigan <35210888+robigan@users.noreply.github.com>
2023-05-22 08:38:42 +0200

    Fuck this shit I am out

--- main.cpp
+++ main.cpp
@@ -4,14 +4,18 @@
 
 using namespace std;
 
-struct chess {
+struct Chess
+{
     char board[8][8];
     string player_names[2];
 
-    chess() {
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                board[i][j] = '0';
+    Chess()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                board[i][j] = ' ';
             }
         }
 
@@ -19,57 +23,133 @@ struct chess {
         player_names[1] = "no name";
     }
 
-    void initialize() {
+    void initialize()
+    {
         char layout1[] = "rnbqkbnr";
         char layout2[] = "pppppppp";
-        
+
         char layout3[] = "PPPPPPPP";
         char layout4[] = "RNBQKBNR";
 
-        for (int j = 0; j < 8; j++) {
+        for (int j = 0; j < 8; j++)
+        {
             board[0][j] = layout1[j];
         }
-        
-        for (int j = 0; j < 8; j++) {
+
+        for (int j = 0; j < 8; j++)
+        {
             board[1][j] = layout2[j];
         }
-        
-        for (int j = 0; j < 8; j++) {
+
+        for (int j = 0; j < 8; j++)
+        {
             board[6][j] = layout3[j];
         }
-        
-        for (int j = 0; j < 8; j++) {
+
+        for (int j = 0; j < 8; j++)
+        {
             board[7][j] = layout4[j];
         }
     }
 
-    void ls() {
-        cout << "    a   b   c   d   e   f   g   h";
-        cout << "  --------------------------------";
-        cout << "8 |   |   |   |   |   |   |   |  | 8";
-        
+    void move(const int x, const int y, const int i, const int j) {
+        board[i][j] = board[x][y];
+        board[x][y] = ' ';
     }
-    
-    void print_players() {
-        cout << "Player 1's name is " << player_names[0] << endl << "Player 2's name is " << player_names[1] << endl;
+
+    void print_players()
+    {
+        cout << "Player 1's name is " << player_names[0] << endl
+             << "Player 2's name is " << player_names[1] << endl;
     }
 };
 
-int main() {
-    chess C;
-    
-    C.initialize();
-    
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            cout << C.board[i][j];
-        }
-        cout << endl;
+class Renderer
+{
+private:
+    Chess * chess;
+
+    void render_header_row()
+    {
+        cout << "    a   b   c   d   e   f   g   h" << endl;
+        cout << "  ---------------------------------" << endl;
     }
+
+    void render_data_row(const char (&row)[8], int &i)
+    {
+        cout << i + 1 << " | ";
+
+        for (int j = 0; j < 8; j++)
+        {
+            cout << row[j];
+            cout << " | ";
+        }
+
+        cout << i + 1 << endl;
+    }
+
+    void render_footer_row()
+    {
+        cout << "  ---------------------------------" << endl;
+        cout << "    a   b   c   d   e   f   g   h" << endl;
+    }
+
+public:
+    void render() {
+        render_header_row();
+
+        for (int i = 0; i < 8; i++) {
+            render_data_row(chess->board[i], i);
+        }
+
+        render_footer_row();
+    }
+
+    void ask_for_movement() {
+        string piece;
+        string moveTo;
+        int x;
+        int y;
+        int i;
+        int j;
+
+        cout << "Piece to move: ";
+        cin >> piece;
+        cout << "Move to: ";
+        cin >> moveTo;
+
+        if (97 <= piece[0] <= 104) x = piece[0] - 97;
+        if (49 <= piece[1] <= 57) y = piece[1] - 49;
+
+        if (97 <= moveTo[0] <= 104) i = piece[0] - 97;
+        if (49 <= moveTo[1] <= 57) j = piece[1] - 49;
+
+        chess->move(x,y,i,j);
+    }
+
     
+    Renderer(Chess c) {
+        chess = &c;
+    }
+};
+
+int main()
+{
+    Chess C;
+    Renderer renderer(C);
+
+    C.initialize();
+
     cout << endl;
-    
+
     C.print_players();
-    
+
+    renderer.render();
+
+    while (true) {
+        renderer.ask_for_movement();
+        renderer.render();
+    }
+
     return 0;
 }
